bd7a4b2e7ad3795af27f7e4c281fc33c17dad21c
ozz <ozik.dev@gmail.com>
2019-05-13 16:24:24 +0500

    - update camera configs - more control over drivers

--- drivers.py
+++ drivers.py
@@ -1,83 +1,113 @@
 from gpiozero import DigitalInputDevice, DistanceSensor, Motor
+from functools import partial as bind
 
-import sys, cv2, time
+sensors = {
+    # "ir": {
+    #     "left": DigitalInputDevice(12),
+    #     "right": DigitalInputDevice(16)
+    # },
+    # "trace": {
+    #     "left": DigitalInputDevice(7),
+    #     "right": DigitalInputDevice(8)
+    # },
+    # "distance": DistanceSensor(26, 19),
+    # "motor": {
+    #     'left': Motor(18, 23),
+    #     'right': Motor(24, 25)
+    # }
+}
 
-rightIRSensor = DigitalInputDevice(12)
-leftIRSensor = DigitalInputDevice(16)
-
-# IR_tracer      8   L
-#               7   R
-rightIRTracerSensor = DigitalInputDevice(7)
-leftIRTracerSensor = DigitalInputDevice(8)
-
-# DisatanceSensor pin   21 T
-#                      20 E
-distanceSensor = DistanceSensor(26, 19)
-
-# Motors
-motor_left = Motor(18, 23)
-motor_right = Motor(24, 25)
-
-forwardpin = False
+forwardPin = False
 
 
-def getDistance():
-    return distanceSensor.distance
+def get_distance():
+    return sensors["distance"].distance
 
 
 def forward(left=1.0, right=1.0):
-    motor_left.forward(left)
-    motor_right.forward(right)
+    sensors["motor"]["left"].forward(left)
+    sensors["motor"]["right"].forward(right)
 
 
 def back(left=1.0, right=1.0):
-    motor_left.backward(left)
-    motor_right.backward(right)
+    sensors["motor"]["left"].backward(left)
+    sensors["motor"]["right"].backward(right)
 
 
-def turnRight(x=1.0, y=1.0):
-    motor_left.forward(x)
-    motor_right.backward(y)
+def turn_right(x=1.0, y=1.0):
+    sensors["motor"]["left"].forward(x)
+    sensors["motor"]["right"].backward(y)
 
 
-def turnLeft(back=1.0, forward=1.0):
-    motor_left.backward(back)
-    motor_right.forward(forward)
+def turn_left(back=1.0, forward=1.0):
+    sensors["motor"]["left"].backward(back)
+    sensors["motor"]["right"].forward(forward)
 
 
 def stop():
-    motor_left.stop()
-    motor_right.stop()
+    sensors["motor"]["left"].stop()
+    sensors["motor"]["right"].stop()
 
 
 def lIRsensor():
-    return rightIRSensor.value
+    return sensors["ir"]["left"].value
 
 
 def rIRsensor():
-    return leftIRSensor.value
+    return sensors["ir"]["right"].value
 
 
 def rIRTracerSensor():
-    return rightIRTracerSensor.value
+    return sensors["trace"]["left"].value
 
 
 def lIRTracerSensor():
-    return leftIRTracerSensor.value
+    return sensors["trace"]["left"].value
 
+
+motor_configs = {}
+
+for side in ["left", "right", "forward", "back"]:
+    motor_configs[side + ".x"] = 1
+    motor_configs[side + ".y"] = 1
 
 if __name__ == "__main__":
+    import cv2
+    import os, time
+
+    width = 640
+    height = 480
+
+    if not os.path.exists('/dev/video0'):
+        path = 'sudo modprobe bcm2835-v4l2 max_video_width=640 max_video_height=480'
+        os.system(path)
+
     allowed = {
         ord("w"): forward,
         ord("s"): back,
-        ord("a"): turnLeft,
-        ord("d"): turnRight,
+        ord("a"): turn_left,
+        ord("d"): turn_right,
         ord("f"): stop
     }
 
     print("Allowed only: {}".format(allowed.keys()))
 
+    cv2.namedWindow("camera")
+
     camera = cv2.VideoCapture(0)
+    camera.set(3, width)
+    camera.set(4, height)
+
+    def set_motor_config(config_name, value):
+        motor_configs[config_name] = value / 100
+
+    for config in motor_configs:
+        update_config = bind(set_motor_config, config)
+
+        cur_value = motor_configs[config] * 100
+        cv2.createTrackbar(config, "camera", cur_value, 100, update_config)
+        cv2.setTrackbarMin(config, "camera", -100)
+        cv2.setTrackbarMax(config, "camera", 100)
 
     while camera.isOpened():
         _, frame = camera.read()
@@ -95,4 +125,4 @@ if __name__ == "__main__":
             exit(0)
 
         time.sleep(0.1)
-        stop()
+        # stop()
