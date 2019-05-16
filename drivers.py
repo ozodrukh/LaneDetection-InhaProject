3d09d17851514be9fee25f4ba79d80daaa3cb752
Haorui <353630611@qq.com>
2019-05-17 02:05:49 +0500

    add parking

--- drivers.py
+++ drivers.py
@@ -162,7 +162,28 @@ def obstacle_detection(angel):
         stop()
 
     # forward()
-
+def left_parking():
+    motor_configs["forward.y"] = 1
+    motor_configs["forward.x"] = 1
+    forward()
+    time.sleep(0.1)
+    motor_configs["forward.y"] = 1
+    motor_configs["forward.x"] = 0.23
+    forward()
+    time.sleep(1.5)
+    stop()
+    time.sleep(5)
+def right_parking():
+    motor_configs["forward.y"] = 1
+    motor_configs["forward.x"] = 1
+    forward()
+    time.sleep(0.1)
+    motor_configs["forward.y"] = 0.23
+    motor_configs["forward.x"] = 1
+    forward()
+    time.sleep(1.5)
+    stop()
+    time.sleep(5)
 
 def on_road_detected(direction, angel):
     print(direction, angel)
