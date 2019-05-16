da4376b351971bc307d9702faa7b2198127decb9
Haorui <353630611@qq.com>
2019-05-17 02:09:48 +0500

    add parking with distance

--- drivers.py
+++ drivers.py
@@ -162,28 +162,63 @@ def obstacle_detection(angel):
         stop()
 
     # forward()
+
+
 def left_parking():
-    motor_configs["forward.y"] = 1
-    motor_configs["forward.x"] = 1
-    forward()
-    time.sleep(0.1)
-    motor_configs["forward.y"] = 1
-    motor_configs["forward.x"] = 0.23
-    forward()
-    time.sleep(1.5)
-    stop()
-    time.sleep(5)
+    if get_distance() < 0.25:
+        motor_configs["forward.y"] = 1
+        motor_configs["forward.x"] = 1
+        forward()
+        time.sleep(0.1)
+        motor_configs["forward.y"] = 1
+        motor_configs["forward.x"] = 0.23
+        forward()
+        time.sleep(1.5)
+        stop()
+        time.sleep(5)
+
+
 def right_parking():
-    motor_configs["forward.y"] = 1
-    motor_configs["forward.x"] = 1
-    forward()
-    time.sleep(0.1)
-    motor_configs["forward.y"] = 0.23
-    motor_configs["forward.x"] = 1
-    forward()
-    time.sleep(1.5)
-    stop()
-    time.sleep(5)
+    if get_distance() < 0.25:
+        motor_configs["forward.y"] = 1
+        motor_configs["forward.x"] = 1
+        forward()
+        time.sleep(0.1)
+        motor_configs["forward.y"] = 0.23
+        motor_configs["forward.x"] = 1
+        forward()
+        time.sleep(1.5)
+        stop()
+        time.sleep(5)
+
+
+def right_parking_distance():
+    if get_distance() < 0.25:
+        motor_configs["forward.y"] = 1
+        motor_configs["forward.x"] = 1
+        forward()
+        time.sleep(0.1)
+        motor_configs["forward.y"] = 0.23
+        motor_configs["forward.x"] = 1
+        forward()
+        time.sleep(1.5)
+        stop()
+        time.sleep(5)
+
+
+def left_parking_distance():
+    if get_distance() < 0.25:
+        motor_configs["forward.y"] = 1
+        motor_configs["forward.x"] = 1
+        forward()
+        time.sleep(0.1)
+        motor_configs["forward.y"] = 1
+        motor_configs["forward.x"] = 0.23
+        forward()
+        time.sleep(1.5)
+        stop()
+        time.sleep(5)
+
 
 def on_road_detected(direction, angel):
     print(direction, angel)
