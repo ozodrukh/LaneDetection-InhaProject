33f1bed8225072b00ebee69deb3c69c762d0faa1
ozz <ozik.dev@gmail.com>
2019-05-17 12:11:27 +0500

    - modifications

--- traffic_sign_classification.py
+++ traffic_sign_classification.py
@@ -13,23 +13,24 @@ HSV_RED_LOWER = np.array([-10, 50, 50])
 HSV_RED_UPPER = np.array([10, 255, 255])
 
 
-def try_detect_sign(img):
-    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+def try_detect_sign(frame):
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
     mask_blue = cv2.inRange(hsv, HSV_BLUE_LOWER, HSV_BLUE_UPPER)
     mask_red = cv2.inRange(hsv, HSV_RED_LOWER, HSV_RED_UPPER)
 
     if GUI_MODE:
-        cv2.imshow('frame', img)
+        cv2.imshow('frame', frame)
         cv2.imshow('color_space', hsv)
-        cv2.imshow('mask', mask_blue)
+        cv2.imshow('blue_mask', mask_blue)
+        cv2.imshow('red_mask', mask_red)
 
-    res, i = filter_noise(mask_blue, img, 0)
-    res, i = filter_noise(mask_red, img, i)
+    res, i = filter_noise(mask_blue, frame, 0)
+    res, i = filter_noise(mask_red, frame, i)
     return res
 
 
-def filter_noise(mask, img, i):
+def filter_noise(mask, frame, i):
     target = cv2.blur(mask, (9, 9))
 
     _, target = cv2.threshold(target, 127, 255, cv2.THRESH_BINARY)
@@ -42,10 +43,10 @@ def filter_noise(mask, img, i):
     erode = cv2.erode(closed, None, iterations=4)
     dilate = cv2.dilate(erode, None, iterations=10)
 
-    return classify_image(dilate, img, i)
+    return classify_image(dilate, frame, i)
 
 
-def classify_image(dilate, img, i):
+def classify_image(dilate, frame, i):
     import os
 
     parent_dir = "/data/trained_data/"
@@ -56,7 +57,7 @@ def classify_image(dilate, img, i):
 
     contours, hierarchy = cv2.findContours(dilate.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
-    res = img.copy()
+    res = frame.copy()
     for con in contours:
         rect = cv2.minAreaRect(con)
         box = np.int0(cv2.boxPoints(rect))
@@ -69,7 +70,7 @@ def classify_image(dilate, img, i):
         l2 = min([box][0][0][0], [box][0][1][0], [box][0][2][0], [box][0][3][0])
 
         if h1 - h2 > 0 and l1 - l2 > 0:
-            temp = img[h2:h1, l2:l1]
+            temp = frame[h2:h1, l2:l1]
             try:
                 if GUI_MODE:
                     cv2.imshow(str(i), temp)
@@ -88,17 +89,17 @@ def classify_image(dilate, img, i):
     return res, i
 
 
-def with_mask(frame, mask2):
-    return cv2.bitwise_or(frame, mask2)
+def with_mask(frame, mask):
+    return cv2.bitwise_or(frame, mask)
 
 
-def has_similarities(cascade, gray, img, i=0):
+def has_similarities(cascade, gray, frame, i=0):
     found = cascade.detectMultiScale(gray, 1.3, 5)
 
     if len(found) is not 0:
         for (x, y, w, h) in found:
-            cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
-            roi_color = img[y:y + h, x:x + w]
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
+            roi_color = frame[y:y + h, x:x + w]
 
             i += 1
 
